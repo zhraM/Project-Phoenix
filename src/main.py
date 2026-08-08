6a08130c7ce8484e2790aa8215000e1042cf8e0b
Zahra Mohammadiyan <zaram1831@gmail.com>
2026-08-08 20:09:28 +0330

    Add Member model

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from models.book import Book
-
+from models.member import Member
 def main():
     book = Book(
         "Clean Code",
@@ -17,6 +17,9 @@ def main():
     book.display_info()
     
     book.return_book()
+    
+    member = Member("Shokat", 103)
+    member.display_info()
     
     
 if __name__ == "__main__":
---
+++ src/models/member.py
@@ -0,0 +1,7 @@
+class Member:
+    def __init__(self, name, member_id):
+        self.name = name
+        self.member_id = member_id
+    def display_info(self):
+        print(f"Name: {self.name}")
+        print(f"Member ID: {self.member_id}")
